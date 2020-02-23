375ae8d820919c4914b8ea82699448d3684c1e51
Andrija Jovanovic <46319817+andrijaJ01@users.noreply.github.com>
2020-02-23 01:03:48 +0100

    Update engine.py

--- engine.py
+++ engine.py
@@ -51,7 +51,7 @@ def main():
 
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
-    libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
+    libtcod.console_init_root(screen_width, screen_height, 'Dungeon Crawl v0.01', False)
 
     con = libtcod.console_new(screen_width, screen_height)
     panel = libtcod.console_new(screen_width, panel_height)
